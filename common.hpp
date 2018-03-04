647c445942ca9982500f86e0c488a71778fad07c
Tynesha Pham <l.tynesha@gmail.com>
2018-03-03 18:20:00 -0800

    Added Node and Tree class

--- common.hpp
+++ common.hpp
@@ -22,4 +22,51 @@ public:
     void setY(int y) { this->y = y; }
 };
 
+class Node {
+public:
+	Move* move;
+	int score;
+	vector<Node*> next_moves;
+	Node()
+	{
+		move = nullptr;
+	}
+	Node(Move *m)
+	{
+		move = m;
+	}
+	void addNextMove(Move *next)
+	{
+		next_moves.push_back(next);
+	}
+	~Node()
+	{
+		delete move;
+	}
+};
+
+class Tree {
+public:
+	Node* head;
+	Tree()
+	{
+		head = nullptr;
+	}
+	~Tree
+	{
+		deleteTree(head);
+	}
+	void deleteTree(Node *head)
+	{
+		for(int i = 0; i < next_moves.size(); i++)
+		{
+			deleteTree(next_moves[i]);
+		}
+		delete head;
+	}
+	void addNode(Move *m, int score)
+	{
+		
+	}
+}
 #endif
