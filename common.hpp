8c632a8a068518b2fc3e44ea237058fe178a4fac
Tynesha Pham <l.tynesha@gmail.com>
2018-03-04 01:13:20 -0800

    Removed Tree class

--- common.hpp
+++ common.hpp
@@ -27,7 +27,7 @@ public:
 class Node {
 public:
 	Move* move;
-	int score;
+	double score;
 	vector<Node*> next_moves;
 	Move* original_move;
 	Node()
@@ -51,27 +51,4 @@ public:
 		delete move;
 	}
 };
-
-/*
-class Tree {
-public:
-	Node* head;
-	Tree()
-	{
-		head = nullptr;
-	}
-	~Tree
-	{
-		deleteTree(head);
-	}
-	void deleteTree(Node *head)
-	{
-		for(int i = 0; i < next_moves.size(); i++)
-		{
-			deleteTree(next_moves[i]);
-		}
-		delete head;
-	}
-}
-*/
 #endif
