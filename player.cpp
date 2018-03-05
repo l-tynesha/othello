45938defe28d87c2a73890cc84c4e2195e8a383d
DawnTang1999 <dawntang1999@gmail.com>
2018-03-04 17:17:11 -0800

    Segmentation fault hajima SimplePlayer hajima win Constanttimeplayer stiil wins

--- player.cpp
+++ player.cpp
@@ -44,8 +44,7 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
     /*
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
-<<<<<<< HEAD
-     */
+*//*
     if(opponentsMove != nullptr)
 		board->doMove(opponentsMove, op_side);
 	
@@ -55,7 +54,6 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
 	if(bestmove != nullptr)
 		board->doMove(bestmove, pl_side);
 	return bestmove;
-	/*
     vector<Move*> legal_moves;
     for(int x = 0; x < 8; x++)
     {
@@ -119,8 +117,6 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
 	else
 		return nullptr;
 	*/
-
-     */	
 	//int depth = 1
 	int depth = 2;
 	if(testingMinimax)
@@ -176,10 +172,10 @@ Node *Player::minimax(vector<Move*>* moves, Board* b, int depth, Side side)
 	{
 		Board *copy = b->copy();
 		copy->doMove((*moves).at(i), side);
-		vector<Move*>* next = board->getLegalMoves(pl_side); 
+		vector<Move*>* next = copy->getLegalMoves(getOppositeSide(side)); 
 		if((*next).size() == 0)
 			return new Node((*moves).at(i));
-		Node *n = minimax(copy->getLegalMoves(getOppositeSide(side)), copy, depth - 1, getOppositeSide(side));
+		Node *n = minimax(next, copy, depth - 1, getOppositeSide(side));
 		int score = n->score;
 		if(best_move == nullptr)
 		{
@@ -201,96 +197,6 @@ Node *Player::minimax(vector<Move*>* moves, Board* b, int depth, Side side)
 	return best_move;
 }
 
-/*
-Node *Player::minimax(Node *n, Board* b, int depth, Side side)
-{
-	if(depth == 1)
-	{
-		Board *copy = b->copy();
-		copy->doMove(n->move, side);
-		n->score = b->getScore(pl_side);
-		delete copy;
-		//std::cerr << "op(" << n->move->x << ", " << n->move->y << ")";
-		return n;
-	}
-	
-	Node *best_move = nullptr;
-	Board *copy = b->copy();
-	if(n->move != nullptr)
-		copy->doMove(n->move, side);
-	vector<Move*>* next = b->getLegalMoves(getOppositeSide(side));
-	std::cerr << (*next).size() << std::endl;
-	if(side == pl_side)
-	{
-		for(unsigned int i = 0; i < (*next).size(); i++)
-		{
-			
-			Move *move = (*next).at(i);
-			std::cerr << "op(" << move->x << ", " << move->y << ")";
-			Node *m = minimax(new Node((*next).at(i)), b, depth - 1, getOppositeSide(side));
-			if(best_move == nullptr)
-				best_move = m;
-			else if(m!= nullptr && m->score < best_move->score)
-				best_move = m;
-		}
-	}
-	else
-	{
-		for(unsigned int i = 0; i < (*next).size(); i++)
-		{
-			Move *move = (*next).at(i);
-			std::cerr << "pl(" << move->x << ", " << move->y << ")";
-			Node *m = minimax(new Node((*next).at(i)), b, depth - 1, getOppositeSide(side));
-			if(best_move == nullptr)
-				best_move = m;
-			else if(m!= nullptr && m->score > best_move->score)
-				best_move = m;
-		}
-	}
-	std::cerr << std::endl;
-	delete copy;
-	n->score = best_move->score;
-	n->move = best_move->move;
-	return n;
-}*/
-/*
-Node *Player::minimax(Node *n, int depth, Side side)
-{
-	vector<Node*> next = n->next_moves;
-	if(next.size() == 0 || depth == 0)
-		return n;
-	
-	Node *best_move = nullptr;
-<<<<<<< HEAD
-=======
-	//if(!testingMinimax || (side == pl_side && testingMinimax))
->>>>>>> 14eb3e1c88fdaf5635cc1baa75b5fee8651a7772
-	if(side == pl_side)
-	{
-		for(unsigned int i = 0; i < next.size(); i++)
-		{
-			Node * m = minimax(next.at(i), depth - 1, getOppositeSide(side));
-			if(best_move == nullptr)
-				best_move = m;
-			else if(m != nullptr && m->score > best_move->score)
-				best_move = m;
-		}
-		return best_move;
-	}
-	else
-	{
-		for(unsigned int i = 0; i < next.size(); i++)
-		{
-			Node * m = minimax(next.at(i), depth - 1, getOppositeSide(side));
-			if(best_move == nullptr)
-				best_move = m;
-			else if(m != nullptr && m->score < best_move->score)
-				best_move = m;
-		}
-		return best_move;
-	}
-}
-*/
 /*
  * Get opposite side
  */
