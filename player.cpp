e1e1285295ab1e63aafb7b0b4f4e13b295bd02d0
DawnTang1999 <dawntang1999@gmail.com>
2018-03-13 03:17:09 -0700

    Changed to 1 and added depth to 7

--- player.cpp
+++ player.cpp
@@ -34,7 +34,7 @@ Player::~Player() {
  */
 Move *Player::doMove(Move *opponentsMove, int msLeft) {
 	
-	int depth = 8;
+	int depth = 7;
 	if(testingMinimax)
 		depth = 2;
 	if(opponentsMove != nullptr)
